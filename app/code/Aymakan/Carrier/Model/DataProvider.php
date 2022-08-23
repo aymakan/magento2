0a82330f5f5a7dac2e594c0f7a078f6399fd9b45
Abdul Shakoor <abdulshaqoor@gmail.com>
2022-08-23 13:02:22 +0300

    Al Majed Oud

---
+++ app/code/Aymakan/Carrier/Model/DataProvider.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * File Name: DataProvider.php
+ * Created by Altaf Hussain
+ * User: Altaf Hussain
+ * Description: Data Provider
+ * Date: 19 July 2020
+ * Copyright ©Aymakan, Inc. All rights reserved
+ */
+
+namespace Aymakan\Carrier\Model;
+
+use Magento\Ui\DataProvider\AbstractDataProvider;
+
+class DataProvider extends AbstractDataProvider
+{
+
+    public function getData()
+    {
+        return [];
+    }
+}
